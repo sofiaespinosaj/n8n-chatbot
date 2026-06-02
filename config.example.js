3c204d303a543ecc66066a3ccb51a1156956dee9
sofiaespinosaj <espinosaj.sofia@gmail.com>
2026-06-01 20:15:09 -0500

    feat(html): add basic layout for ticket form

--- config.example.js
+++ config.example.js
@@ -1,3 +1,3 @@
 const CONFIG = {
-    N8N_WEBHOOK_URL: 'https://tu-instancia-n8n.cloud/webhook/tu-ruta-aqui'
+    N8N_WEBHOOK_URL: 'https://espinosajsofia-instancia-n8n.cloud/webhook/tu-ruta-aqui'
 };
